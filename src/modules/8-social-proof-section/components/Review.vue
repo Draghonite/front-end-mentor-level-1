03ecf415aa8f112fb0b8aea20c4f3e7c0938f9fd
Ivan Mercedes <ivan.mercedes@sidemotion.com>
2021-11-30 03:14:04 +0000

    accessbility fix

--- src/modules/8-social-proof-section/components/Review.vue
+++ src/modules/8-social-proof-section/components/Review.vue
@@ -1,7 +1,7 @@
 <template>
     <div v-if="review" class="review card">
         <div class="card-header">
-            <div class="picture"><img :src="'data:'+review.Reviewer.Picture.mime+';base64, '+review.Reviewer.Picture.data" :alt="review.Reviewer.Name" /></div>
+            <div class="picture"><img :src="'data:'+review.Reviewer.Picture.mime+';base64,'+review.Reviewer.Picture.data" :alt="review.Reviewer.Name" /></div>
             <div>
                 <div class="name">{{review.Reviewer.Name}}</div>
                 <div class="status">{{review.Reviewer.Status}}</div>
